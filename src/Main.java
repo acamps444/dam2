ea9759995f94cd0f87d702484f4424ab4cb7b04f
acamps444 <adriacamps@inspladelestany.cat>
2022-11-09 17:39:36 +0100

    test

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.sql.*;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+    } // tanca main
+} // tanca class main
